8e27522cec59948349ab97db59ad15f13141c82c
Kevin Jeffers <jevinkeffers@gmail.com>
2020-08-03 13:28:08 -0400

    "Solved problem of changing random number"

--- KJ_Python_101_lessons/Large/GUESS_A_NUMBER.py
+++ KJ_Python_101_lessons/Large/GUESS_A_NUMBER.py
@@ -1,7 +1,7 @@
-# 3. Guess a Number
-# You will implement a guess-the-number game where the player has to try guessing a secret number until they gets it right. For now, you will "hard code" the secret number to 5 (just set it to five like secret_number = 5). You will prompt the player to enter a number again and again, each time comparing their input to the secret number. To to that, you will need to write a while loop. If they guess correctly, you will print "You win!", otherwise, you will prompt for a number again.
+# # 3. Guess a Number
+# # You will implement a guess-the-number game where the player has to try guessing a secret number until they gets it right. For now, you will "hard code" the secret number to 5 (just set it to five like secret_number = 5). You will prompt the player to enter a number again and again, each time comparing their input to the secret number. To to that, you will need to write a while loop. If they guess correctly, you will print "You win!", otherwise, you will prompt for a number again.
 
-#STEP 1
+# #STEP 1
 
 n = 5
 print("I am thinking of a number between 1 and 10.")
@@ -12,7 +12,7 @@ while i != n:
     int(input("Nope, try again. "))
     print("Yes! You win!")
     break
-#SOLVED 
+# #SOLVED 
 
 #STEP 2: Give High-Low Hint
 #Improve your game to provide the player with a high-or-low hint.
@@ -31,13 +31,13 @@ while guess != n:
     if guess == n:
         print("Yes! You win!")
         break
-#SOLVED
+# #SOLVED
 
-# Step 3: Randomly Generated Secret Number
-# Instead of hard-coding the secret number to 5 now, you will generate the secret number using a random number generator provided by Python, so that even you, the programmer, cannot know the secret number before hand.
+# # Step 3: Randomly Generated Secret Number
+# # Instead of hard-coding the secret number to 5 now, you will generate the secret number using a random number generator provided by Python, so that even you, the programmer, cannot know the secret number before hand.
 
-import random
-my_random_number = random.randint(1, 10)
+# import random
+# my_random_number = random.randint(1, 10)
 
 print("I am thinking of a number between 1 and 10.")
 guess = int(input("What's the number? "))
@@ -53,10 +53,10 @@ while guess != my_random_number:
     if guess == my_random_number:
         print("Yes! You win!")
         break
-# #SOLVED
+# # #SOLVED
 
-# Step 4: Limit Number of Guesses
-# Limit the number of guesses the player has to 5. If he cannot guess the number within 5 guesses, he loses. Changing random number range to 1-20.
+# # Step 4: Limit Number of Guesses
+# # Limit the number of guesses the player has to 5. If he cannot guess the number within 5 guesses, he loses. Changing random number range to 1-20.
 
 import random
 my_random_number = random.randint(1, 20)
@@ -80,15 +80,15 @@ if(number_of_guesses - 1) == guesses_made:
 else:
     print("Yes! You win!")
 
-# #SOLVED
+# # #SOLVED
 
 # Bonus: Play Again
 # At the conclusion of a game, give the player the option of playing again.
 
 import random
-my_random_number = random.randint(1, 20)
 
-def number_game(my_random_number):
+def number_game():
+    my_random_number = random.randint(1, 20)
     print("I am thinking of a number between 1 and 20.")
     print("You have 5 guesses left.")
     guess = int(input("What's the number? "))
@@ -109,17 +109,13 @@ def number_game(my_random_number):
         if play_again == 'N':
             print("Bye.")
         if play_again == 'Y':
-            import random
-            number_game(my_random_number)
+            number_game()
     else:
         print("Yes! You win!")
         play_again = input("Would you like to play again? (Y or N) ").upper()
         if play_again == 'N':
             print("Bye.")
         if play_again == 'Y':
-            import random
-            number_game(my_random_number)
+            number_game()
 
-number_game(my_random_number)
-
-#CAN'T FIGURE OUT HOW TO CHANGE THE RANDOM NUMBER ON THE SUBSEQUENT PLAY-THROUGHS
+number_game()
